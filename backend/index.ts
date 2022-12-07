5c921d67a2d3b8e53ebdaa2008438dd1eebbadf2
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-07 18:56:17 +0000

    Add CORS to the backend server

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
